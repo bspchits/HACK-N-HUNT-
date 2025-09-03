4276dfc2f9bd1bcf8df1cac4399ddadf47765a08
BLUE SCREEN PROGRAMMING CLUB <bspc.hits@gmail.com>
2025-09-03 09:37:52 +0530

    Update Hack'N HUNT.py

--- Hack'N HUNT.py
+++ Hack'N HUNT.py
@@ -1,18 +1,19 @@
+#!pip install qrcode
 import qrcode
 import base64
 import pandas as pd
 
 # 🔹 DECODE THE PROGRAM TO GET THE QRCODE
-file_url = .#search
+file_url = .#search the location
 
 # Load Excel file from GitHub
-df = pd___read_excel(file_url)
+df = pd.read_excel(file_url)
 
 # get your team number given in the paper before and replace it with 0 eg:[1,2,3....29,30]
 encodedsecret = df.loc[0, 'encoded']
 
 
-secret_word = base64b64decode(encoded_secret).decode("utf-8")
+secret_word = base64.b64decode(encoded_secret).decode("utf-8")
 
 # Create_QR
 qr = qrcode.QRCode(box_size=8, border=3)
@@ -22,7 +23,8 @@ qr.make(fit='True')
 img = qrmake_image(fill_color="black", back_color="white")
 
 # Show(QR) in Colab
-img.show()
+display(img)
 #r"https://github.com/bspchits/HACK-N-HUNT-/blob/main/ENCODED%20WORDS.xlsx?raw=true"
+
 
 
